135376e7e1d73fc36413b98e6c1cd89a434568ec
Josh Buchea <verctrl@joshbuchea.com>
2020-05-07 18:21:55 -0700

    shorten title

--- config/website.js
+++ config/website.js
@@ -1,8 +1,8 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/notes your pathPrefix should be "notes"
-  siteTitle: "Josh Buchea's digital garden", // Navigation and Site Title
-  siteTitleAlt: 'The site of a digital creator', // Alternative Site title for SEO
-  siteTitleShort: "Josh Buchea's digital garden", // short_name for manifest
+  siteTitle: 'Josh Buchea', // Navigation and Site Title
+  siteTitleAlt: "Josh Buchea's digital garden", // Alternative Site title for SEO
+  siteTitleShort: 'Josh Buchea', // short_name for manifest
   siteUrl: 'https://joshbuchea.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: 'images/avatar.png', // Used for SEO and manifest, path to your image you placed in the 'static' folder
@@ -12,7 +12,7 @@ module.exports = {
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
   userTwitter: '@joshbuchea', // Twitter Username
-  ogSiteName: "Josh Buchea's digital garden", // Facebook Site Name
+  ogSiteName: 'Josh Buchea', // Facebook Site Name
   ogLanguage: 'en_US',
   googleAnalyticsID: 'UA-33680175-1',
 
